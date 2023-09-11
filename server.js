ff3d67695985f143dd597c9c4050793bb5b98a3f
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-09-11 18:19:24 +0100

    changes for prod

--- server.js
+++ server.js
@@ -25,19 +25,19 @@ cloudinary.config({
 })
 
 
-// create server
-const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT}`
-  );
-});
+// // create server
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(
+//     `Server is running on http://localhost:${process.env.PORT}`
+//   );
+// });
 
-// unhandled promise rejection
-process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
+// // unhandled promise rejection
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Shutting down the server for ${err.message}`);
+//   console.log(`shutting down the server for unhandle promise rejection`);
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
