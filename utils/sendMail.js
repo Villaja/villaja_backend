3a13786d0dfc19fa77fcf8a90683d7c8a4cdc0cf
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2023-09-12 16:04:02 +0100

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -3,7 +3,8 @@ const nodemailer = require("nodemailer");
 const sendMail = async (options) => {
     const transporter = nodemailer.createTransport({
         service:'gmail',
-        secure:true,
+        port:465,
+        secureConnection:true,
         auth:{
             type:"OAuth2",
             user: process.env.SMPT_MAIL,
